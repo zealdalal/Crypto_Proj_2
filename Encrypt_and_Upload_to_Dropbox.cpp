d42c398bdbb8373c8ed549290ba302c6b97d5b52
Zeal Dalal <zeal_dalal@yahoo.co.in>
2016-04-18 15:37:10 -0400

    Updated dropbox- Included libraries crypto and hash

--- Encrypt_and_Upload_to_Dropbox.cpp
+++ Encrypt_and_Upload_to_Dropbox.cpp
@@ -3,7 +3,11 @@
 //
 
 #include <iostream>
-#include "/Dropbox-C-master"
+#include "/Dropbox-C-master/Dropbox/include/dropbox.h"
+#include "/Dropbox-C-master/memStream/include/memStream.h"
+#include "/Dropbox-C-master/Dropbox/include/"
+
+using namespace std;
 
 class Encrypt_and_Upload_to_Dropbox
 {
@@ -13,5 +17,15 @@ public:
 
 void Encrypt_and_Upload_to_Dropbox::encryptFunction()
 {
-    // do stuff
+    char *app_key = "your consumer key";
+    char *app_secret = "your consumer secret";
+
+    char *t_key    = NULL; //< access token key
+    char *t_secret = NULL;  //< access token secret
+
+    drbInit();
+
+
+
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 }
