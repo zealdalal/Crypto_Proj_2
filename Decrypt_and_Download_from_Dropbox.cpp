975ff25f29c1bd49a27b54ea1a2c0d28d89b0f0d
Richard <yc2394@nyu.edu>
2016-04-21 14:28:56 -0400

    update download file function

--- Decrypt_and_Download_from_Dropbox.cpp
+++ Decrypt_and_Download_from_Dropbox.cpp
@@ -3,7 +3,9 @@
 //
 
 #include <iostream>
-#include "/Dropbox-C-master"
+#include <string>
+#include "Dropbox-C-master/Dropbox/include/dropbox.h"
+#include "Dropbox-C-master/memStream/include/memStream.h"
 
 using namespace std;
 
@@ -13,15 +15,23 @@ public:
     void decryptFunction();
 private:
     void displayAccountInfo(drbAccountInfo *info);
+    void displayMetadata(drbMetadata *meta, char *title);
+    void displayMetadataList(drbMetadataList* list, char* title);
 };
 
 void Decrypt_and_Download_from_Dropbox::decryptFunction()
 {
+    int err;
+    void *output;
+
     char *app_key = "your consumer key";
     char *app_secret = "your consumer secret";
 
     char *t_key    = NULL; //< access token key
     char *t_secret = NULL;  //< access token secret
+
+    char *filename = NULL;  //name of file to be downloaded
+    string directory = "/Download/";  //default directory
 
     drbInit();
 
@@ -65,7 +75,58 @@ void Decrypt_and_Download_from_Dropbox::decryptFunction()
         displayAccountInfo(info);
         drbDestroyAccountInfo(info);
     }
+
+    // List root directory files
+    output = NULL;
+    err = drbGetMetadata(cli, &output,
+                         DRBOPT_PATH, "/",
+                         DRBOPT_LIST, true,
+            //                     DRBOPT_FILE_LIMIT, 100,
+                         DRBOPT_END);
+    if (err != DRBERR_OK) {
+        printf("Metadata error (%d): %s\n", err, (char*)output);
+        free(output);
+    } else {
+        drbMetadata* meta = (drbMetadata*)output;
+        displayMetadata(meta, "Metadata");
+        drbDestroyMetadata(meta, true);
+    }
+
+    //Getting the filename the user wish to download
+    cout << "Input the name of file you want to download:";
+    cin >> filename;
+    string full_path = directory + filename;
+    FILE *file = fopen(full_path, "w"); // Write it in this file
+    output = NULL;
+    err = drbGetFile(cli, &output,
+                     DRBOPT_PATH, "/hello.txt",
+                     DRBOPT_IO_DATA, file,
+                     DRBOPT_IO_FUNC, fwrite,
+                     DRBOPT_END);
+    fclose(file);
+
+    if (err != DRBERR_OK) {
+        printf("Get File error (%d): %s\n", err, (char*)output);
+        free(output);
+    } else {
+        displayMetadata((drbMetadata*)output, "Get File Result");
+        drbDestroyMetadata((drbMetadata*)output, true);
+    }
+
+
+    //Decrypt the downloaded file
+
+
+
+    // Free all client allocated memory
+    drbDestroyClient(cli);
+
+    // Global Cleanup
+    drbCleanup();
 }
+
+
+
 
 
 
@@ -86,5 +147,50 @@ void Decrypt_and_Download_from_Dropbox::displayAccountInfo(drbAccountInfo* info)
         if(info->quotaInfo.shared)     printf("shared:       %u\n", *info->quotaInfo.shared);
         if(info->quotaInfo.quota)      printf("quota:        %u\n", *info->quotaInfo.quota);
         if(info->quotaInfo.normal)     printf("normal:       %u\n", *info->quotaInfo.normal);
+    }
+}
+
+const char* strFromBool(bool b) { return b ? "true" : "false"; }
+
+/*!
+ * \brief  Display a drbMetadata item in stdout.
+ * \param  meta    metadata to display.
+ * \param   title   display the title before the metadata.
+ * \return  void
+ */
+void Decrypt_and_Download_from_Dropbox::displayMetadata(drbMetadata* meta, char* title) {
+    if (meta) {
+        if(title) printf("---------[ %s ]---------\n", title);
+        if(meta->hash)        printf("hash:        %s\n", meta->hash);
+        if(meta->rev)         printf("rev:         %s\n", meta->rev);
+        if(meta->thumbExists) printf("thumbExists: %s\n", strFromBool(*meta->thumbExists));
+        if(meta->bytes)       printf("bytes:       %d\n", *meta->bytes);
+        if(meta->modified)    printf("modified:    %s\n", meta->modified);
+        if(meta->path)        printf("path:        %s\n", meta->path);
+        if(meta->isDir)       printf("isDir:       %s\n", strFromBool(*meta->isDir));
+        if(meta->icon)        printf("icon:        %s\n", meta->icon);
+        if(meta->root)        printf("root:        %s\n", meta->root);
+        if(meta->size)        printf("size:        %s\n", meta->size);
+        if(meta->clientMtime) printf("clientMtime: %s\n", meta->clientMtime);
+        if(meta->isDeleted)   printf("isDeleted:   %s\n", strFromBool(*meta->isDeleted));
+        if(meta->mimeType)    printf("mimeType:    %s\n", meta->mimeType);
+        if(meta->revision)    printf("revision:    %d\n", *meta->revision);
+        if(meta->contents)    displayMetadataList(meta->contents, "Contents");
+    }
+}
+
+/*!
+ * \brief  Display a drbMetadataList item in stdout.
+ * \param  list    list to display.
+ * \param   title   display the title before the list.
+ * \return  void
+ */
+void Decrypt_and_Download_from_Dropbox::displayMetadataList(drbMetadataList* list, char* title) {
+    if (list){
+        printf("---------[ %s ]---------\n", title);
+        for (int i = 0; i < list->size; i++) {
+
+            displayMetadata(list->array[i], list->array[i]->path);
+        }
     }
 }
